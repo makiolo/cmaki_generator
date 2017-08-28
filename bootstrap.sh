7fdf29f112cc7695ee28f12118f6276f3fb09703
Ricardo <makiolo@gmail.com>
2017-08-28 22:31:56 +0200

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ ! -d node_modules/cmaki_generator ]]; then
-	mkdir -p node_modules/cmaki_generator
-	(cd node_modules && git clone https://github.com/makiolo/cmaki_generator.git)
-	(cd node_modules/cmaki_generator && rm -Rf .git)
-fi
