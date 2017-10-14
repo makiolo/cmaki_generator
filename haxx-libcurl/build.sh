ea084f534353766e51d2da2238145cdec8653466
Ricardo Marmolejo García <makiolo@gmail.com>
2017-10-14 20:15:37 +0200

    .

--- haxx-libcurl/build.sh
+++ haxx-libcurl/build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-source $(pwd)/find.script
-source $(pwd)/../toolchain/find.script
-
-# need use zlib from toolchain
-export PKG_CONFIG_PATH=$toolchain_BASE/lib/pkgconfig
-
-./configure --prefix=$haxx_libcurl_HOME --without-ssl && make -j $CORES && make -j $CORES install
+./configure --prefix=$SELFHOME --without-ssl && make -j $CORES && make -j $CORES install
 
